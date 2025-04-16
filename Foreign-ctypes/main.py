573e077878e278a9b56e0d5650422d460505bf24
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-04-16 20:16:10 +0100

    Agora já funciona e dá o resultado correto.

--- Foreign-ctypes/main.py
+++ Foreign-ctypes/main.py
@@ -12,6 +12,7 @@ print("")
 
 print("############################")
 print("### Demonstrating functions with double:")
+my_functions.square_doubles.restype = ctypes.c_double
 print( my_functions.square_doubles( ctypes.c_double(10.1) ) )
 print( my_functions.square_doubles( ctypes.c_double(8.2)  ) )
 print("")
