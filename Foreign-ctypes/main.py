dbb8c218df1ec84d434fdd22b9444f9aeeb4e185
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-04-16 20:10:40 +0100

    Expandindo para doubles.

--- Foreign-ctypes/main.py
+++ Foreign-ctypes/main.py
@@ -2,10 +2,16 @@ from ctypes import *
 
 ### Importing the object:
 my_functions = CDLL("./my_functions.so")
-
-############################
-### Demonstrating int-only functions:
 print(type(my_functions))
+
+print("############################")
+print("### Demonstrating int-only functions:")
 print(my_functions.square(10))
 print(my_functions.square(8))
+print("")
 
+print("############################")
+print("### Demonstrating functions with double:")
+print(my_functions.square_doubles(10))
+print(my_functions.square_doubles(8))
+print("")
