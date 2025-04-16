246e5ba4652b70f112cbc72a715e841a07913767
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-04-16 20:58:02 +0100

    Avanços.

--- Foreign-cffi/main_abi.py
+++ Foreign-cffi/main_abi.py
@@ -8,20 +8,11 @@ with open(h_file_name) as h_file:
     ffi.cdef(h_file.read())
 ffi.set_source(
     "cffi_example",
-    # Since you're calling a fully-built library directly, no custom source
-    # is necessary. You need to include the .h files, though, because behind
-    # the scenes cffi generates a .c file that contains a Python-friendly
-    # wrapper around each of the functions.
-    '#include "my_functions.h"',
-    # The important thing is to include the pre-built lib in the list of
-    # libraries you're linking against:
-    libraries=["my_functions"],
-    library_dirs=[this_dir.as_posix()],
-    extra_link_args=["-Wl,-rpath,."],
+    None,
     )
 ffi.compile()
 
-
+exit()
 print("############################")
 print("### Demonstrating functions with double:")
 my_functions.square_doubles.restype = ctypes.c_double
